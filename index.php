19d2f199dfd8f8775a54890ab7bcdfc87fecc54c
francisohara24 <franciskohara@gmail.com>
2023-12-30 07:14:24 -0500

    Style: update header text and account type radio buttons.

--- index.php
+++ index.php
@@ -1,46 +1,56 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <title>Login to File Share</title>
+    <title>Welcome to File Share</title>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet"
           integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
-    <link rel="icon" href="https://github.com/francisohara24/PHP-File-Share/blob/master/content/logo-small.jpeg?raw=true">
+    <link rel="icon"
+          href="https://github.com/francisohara24/PHP-File-Share/blob/master/content/logo-small.jpeg?raw=true">
 </head>
 <body class="container">
 <h2 style="text-align: center; font-style: italic">PHP FileShare</h2>
 
 
 <form action="" method="post">
-        <h4>Login</h4>
-        <div class="mb-3 form-floating">
-            <input type="text" class="form-control" id="username" name="username"
-                   value="<?php echo count($_POST) > 0 ? $_POST["username"] : "" ?>"
-                   placeholder="Enter your username" required>
-            <label for="username">Username </label>
+    <h4>Login</h4>
+    <div class="mb-3 form-floating">
+        <input type="text" class="form-control" id="username" name="username"
+               value="<?php echo count($_POST) > 0 ? $_POST["username"] : "" ?>"
+               placeholder="Enter your username" required>
+        <label for="username" class="form-label">Username </label>
 
-        </div>
-        <div class="mb-3 form-floating">
-            <input type="password" class="form-control" id="password" name="password"
-                   value="<?php echo count($_POST) > 0 ? $_POST["password"] : "" ?>"
-                   placeholder="Enter your password" required>
-            <label for="password" class="form-label">Password </label>
-        </div>
-        <div class="mb-3">
-            <label for="employee" class="form-label"> Employee </label>
-            <input type="radio" class="form-check-input" id="employee" checked name="account-type" value="employee" required>
+    </div>
+    <div class="mb-3 form-floating">
+        <input type="password" class="form-control" id="password" name="password"
+               value="<?php echo count($_POST) > 0 ? $_POST["password"] : "" ?>"
+               placeholder="Enter your password" required>
+        <label for="password" class="form-label">Password </label>
+    </div>
 
-            <label for="manager">Manager</label>
-            <input type="radio" class="form-check-input" id="manager" name="account-type" value="manager">
-        </div>
-        <button class="btn btn-primary" type="submit" name="login">Log In</button>
-        <button class="btn btn-primary" type="submit" name="signup" formaction="signup.php">Sign Up</button>
-        <br><br>
-        <?php if (isset($_POST["login"])) authorize($_POST["username"], $_POST["password"]); ?>
+    <div class="form-check form-check-inline">
+        <label for="employee" class="form-check-label"> Employee </label>
+        <input type="radio" class="form-check-input" id="employee" checked name="account-type" value="employee"
+               required>
+    </div>
 
-        <br>
-    <div style="text-align: right">No account? <a href="signup.php">Sign up</a></div>
+    <div class="form-check form-check-inline">
+        <label for="manager" class="form-check-label"> Manager</label>
+        <input type="radio" class="form-check-input" id="manager" name="account-type" value="manager">
+    </div>
+    <br>
+    <br>
+    <div>
+    <button class="btn btn-primary" type="submit" name="login">Log In</button>
+    <button class="btn btn-primary" type="submit" name="signup" formaction="signup.php">Sign Up</button>
+        <div style="text-align: right">No account? <a href="signup.php">Sign up</a></div>
+    </div>
+
+    <?php if (isset($_POST["login"])) authorize($_POST["username"], $_POST["password"]); ?>
+
+    <br>
+
 </form>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"
         integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL"
