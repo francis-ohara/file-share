c01141fcc59b269b21b0a2ed51fefb527053c3e4
francisohara24 <franciskohara@gmail.com>
2024-01-06 09:21:09 -0500

    Feat: update comments and page names.

--- sign-up.php
+++ sign-up.php
@@ -12,10 +12,10 @@
         <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.5.0/font/bootstrap-icons.css" rel="stylesheet" type="text/css" />
         <!-- Google fonts-->
         <link href="https://fonts.googleapis.com/css?family=Lato:300,400,700,300italic,400italic,700italic" rel="stylesheet" type="text/css" />
-        <!-- Custom CSS (includes Bootstrap)-->
+        <!-- Custom CSS (includes Bootstrap) -->
         <link href="css/styles.css" rel="stylesheet" />
     </head>
-    <body>
+    <body style="background-image: url('assets/landing-page-2.jpeg'); background-size: cover; background-repeat: no-repeat; height: 100vh; background-position: center">
         <!-- Navigation-->
         <nav class="navbar navbar-light bg-light static-top">
             <div class="container">
@@ -27,61 +27,26 @@
             </div>
         </nav>
 
-        <!-- Masthead-->
-        <header class="masthead">
+        <!-- Signup Form    -->
+<!--
+    Form content
+        1. Username
+        2. Email
+        3. First Name
+        4. Last Name
+        5. Password
+        6. Confirm password
+        7. Phone Number (for extra security) ## maybe?
+        8. I confirm that the above information is correct and true to my knowledge.
 
-            <div class="container position-relative">
-                <div class="row justify-content-center">
-                    <div class="col-xl-6">
-                        <div class="text-center text-white">
-                            <!-- Page heading-->
-                            <h1 class="mb-5">Share your files securely on the cloud</h1>
-                            <a class="btn btn-primary btn-lg rounded-5" href="sign-up.php">Create a Free Account</a>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </header>
-        <!-- Icons Grid-->
-        <section class="features-icons bg-light text-center">
-            <div class="container">
-                <div class="row">
-                    <div class="col-lg-4">
-                        <div class="features-icons-item mx-auto mb-5 mb-lg-0 mb-lg-3">
-                            <div class="features-icons-icon d-flex"><i class="bi-cloud-check m-auto text-primary"></i></div>
-                            <h3>Secure Storage</h3>
-                            <p class="lead mb-0">Uploaded files are stored securely with Google Cloud Platform.</p>
-                        </div>
-                    </div>
-                    <div class="col-lg-4">
-                        <div class="features-icons-item mx-auto mb-5 mb-lg-0 mb-lg-3">
-                            <div class="features-icons-icon d-flex"><i class="bi-people m-auto text-primary"></i></div>
-                            <h3>Multi-User Support</h3>
-                            <p class="lead mb-0">Allows different users to login and share files.</p>
-                        </div>
-                    </div>
-                    <div class="col-lg-4">
-                        <div class="features-icons-item mx-auto mb-0 mb-lg-3">
-                            <div class="features-icons-icon d-flex"><i class="bi-terminal m-auto text-primary"></i></div>
-                            <h3>Easy to Use</h3>
-                            <p class="lead mb-0">Just create an account, sign in, and start sharing!</p>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </section>
+-->
 
         <!-- Footer -->
-        <footer class="footer bg-dark">
+        <footer class="footer-narrow bg-dark fixed-bottom">
             <div class="container">
                 <div class="row">
                     <div class="col-lg-6 h-100 text-center text-lg-start my-auto">
-                        <ul class="list-inline mb-2">
-                            <li class="list-inline-item"><a href="sign-in.php">Sign In</a></li>
-                            <li class="list-inline-item">⋅</li>
-                            <li class="list-inline-item"><a href="sign-up.php">Sign Up</a></li>
 
-                        </ul>
                         <p class="text-muted small mb-4 mb-lg-0">Copyright &copy; 2024 &nbsp Francis O'Hara. &nbsp&nbsp All Rights Reserved.</p>
                     </div>
                     <div class="col-lg-6 h-100 text-center text-lg-end my-auto">
@@ -100,6 +65,7 @@
                 </div>
             </div>
         </footer>
+
         <!-- Bootstrap core JS-->
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"></script>
         <!-- Custom JS-->
