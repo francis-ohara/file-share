3fcb8a4468997ce7a6b92c84922a9aefa4d9329a
francisohara24 <franciskohara@gmail.com>
2024-01-09 06:47:37 -0500

    Feat: complete frontend for sign up page.

--- sign-up.php
+++ sign-up.php
@@ -15,7 +15,7 @@
         <!-- Custom CSS (includes Bootstrap) -->
         <link href="css/styles.css" rel="stylesheet" />
     </head>
-    <body style="background-image: url('assets/landing-page-2.jpeg'); background-size: cover; background-repeat: no-repeat; height: 100vh; background-position: center">
+    <body style="background-image: url('assets/landing-page-2.jpeg'); background-size: cover; background-repeat: no-repeat; background-position: center">
         <!-- Navigation-->
         <nav class="navbar navbar-light bg-light static-top">
             <div class="container">
@@ -28,21 +28,104 @@
         </nav>
 
         <!-- Signup Form    -->
-<!--
-    Form content
-        1. Username
-        2. Email
-        3. First Name
-        4. Last Name
-        5. Password
-        6. Confirm password
-        7. Phone Number (for extra security) ## maybe?
-        8. I confirm that the above information is correct and true to my knowledge.
+        <!--
+            Form content
+                2. Email
+                3. First Name
+                4. Last Name
+                5. Password
+                6. Confirm password
+                7. Phone Number (for extra security) ## maybe?
+                8. I confirm that the above information is correct and true to my knowledge.
+        -->
 
--->
+
+
+        <div class="container d-flex flex-column align-items-center" style="padding-top: 10vh; padding-bottom: 10vh;">
+            <!-- TODO: Add php code to control which alert gets echoed.
+               Success alert could be dismissible modal with go to sign up page button.
+               Eg. failure message: an account with that email already exists, ensure the passwords you entered match
+            -->
+            <div class="d-none alert alert-danger alert-dismissible fade show" role="alert" id="alert-duplicate-email">
+                An account with that email already exists. Please use a different email.
+                <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+            </div>
+
+            <div class="d-none alert alert-danger alert-dismissible fade show" role="alert" id="alert-password-mismatch">
+                The passwords you entered do not match. Please try again.
+                <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+            </div>
+
+
+            <div class="card w-100 rounded-4 p-5 shadow-lg" style="max-width: 600px; min-width: 500px;">
+                <img src="assets/logo.png"
+                     alt="File Share app logo: a white folder with the letters F and S written on it in blue."
+                     width="120px" class="mb-4 align-self-center">
+
+                <form action="" method="post" class="">
+                    <div class="container border rounded-3 px-5 pb-5 pt-4">
+                    <div class="mb-5 mt-2">
+                        <h1 class="text-center display-6 fs-2">Create an Account</h1>
+                    </div>
+                    <div class="row" >
+                        <!--  div for both first and last name -->
+                        <div class="col">
+                            <div class="form-floating">
+                                <input type="text" class="form-control" id="first-name" name="user[first-name]" placeholder="First name" aria-describedby="name-help" required>
+                                <label for="first-name" class="form-label">First Name</label>
+                            </div>
+                        </div>
+
+                        <div class="col ">
+                            <div class="form-floating">
+                                <input type="text" class="form-control" id="last-name" name="user[first-name]" placeholder="Last name" aria-describedby="name-help" required>
+                                <label for="last-name" class="form-label">Last Name</label>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="form-text mb-4" id="name-help">&nbsp &nbsp Enter your first and last name.</div>
+
+                    <div class="mb-4 form-floating" >
+                        <!--  email -->
+                        <input  type="email" class="form-control" id="email" name="user[email]" placeholder="Email address" aria-describedby="email-help" required>
+                        <label for="email" class="form-label">Email address</label>
+                        <div class="form-text" id="email-help">&nbsp&nbsp Enter the address you will use to log in to your account.</div>
+                    </div>
+
+
+                    <div class="row ">
+                        <div class="col">
+                            <!-- password -->
+                            <div class="form-floating">
+                                <input type="password" class="form-control" id="password" name="user[password]" placeholder="Password" aria-describedby="password-help" required>
+                                <label for="password" class="form-label">Password</label>
+                            </div>
+                        </div>
+
+                        <div class="col">
+                            <!--  confirm password-->
+                            <div class="form-floating">
+                                <input type="password" class="form-control" id="confirm-password" name="user[confirm-password]" placeholder="Confirm password" required>
+                                <label for="confirm-password" class="form-label">Confirm password</label>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="form-text mb-3" id="password-help">&nbsp &nbsp Create a new password for your account.</div>
+
+                    <button class="btn btn-primary mt-3 mb-4 w-100" type="submit">Continue</button>
+
+                        <div class="text-center mb-3">
+                            Already a member? &nbsp <a href="sign-in.php">Sign in</a>
+                        </div>
+                    </div>
+                </form>
+
+
+            </div>
+        </div>
 
         <!-- Footer -->
-        <footer class="footer-narrow bg-dark fixed-bottom">
+        <footer class="bg-dark" style="padding-top: 1rem; padding-bottom: 1rem;">
             <div class="container">
                 <div class="row">
                     <div class="col-lg-6 h-100 text-center text-lg-start my-auto">
@@ -72,3 +155,8 @@
         <script src="js/scripts.js"></script>
     </body>
 </html>
+
+<?php
+    require "backend/helpers.php";
+    // $msg = create_account();
+?>
