008a637e5fd918f9b35ad739c5609f3c27bdcd18
francisohara24 <franciskohara@gmail.com>
2024-01-09 06:53:45 -0500

    Feat: add backend files and directory.

---
+++ backend/helpers.php
@@ -0,0 +1,15 @@
+<?php
+// Helper functions for the app
+
+function authenticate($user){
+    // where user is the user array from post
+    // authorize users trying to log into the application.
+    // figure out way to send post request to dashboard page on success.
+}
+
+// TODO: add other helper functions for the app.
+//  MySQL Tables: Users will have a storage limit field.
+
+function create_account($user){
+    // where user is the user array from post.
+}
