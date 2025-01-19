52fd51991b56393e49209a07f2ac4c19ca5724ff
francisohara24 <franciskohara@gmail.com>
2025-01-19 15:14:43 -0500

    feat: add script for updating footer text

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,3 @@
-// Custom JavaScript for the project.
+// update footer text
+let year = new Date().getFullYear();
+document.getElementById("footer-text").innerHTML = "Copyright &copy; " + year + " &nbsp Francis O'Hara. &nbsp&nbsp All Rights Reserved.";
